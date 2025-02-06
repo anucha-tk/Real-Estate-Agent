58f903fa77efd2201e122242143dbcfe40b2059d
Anucha.tk <anuchataowkaen@gmail.com>
2025-02-06 15:35:10 +0700

    feat: configuration firebase

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 - [Real Estate Agent](#real-estate-agent)
 - [Tool Stack](#tool-stack)
+- [Learn](#learn)
+  - [Setup Firebase Client-side and Server-side](#setup-firebase-client-side-and-server-side)
 
 # Tool Stack
 
@@ -9,3 +11,13 @@
 - Tailwindcss(v3)
 - Typescript
 - Firebase
+- Auth
+  - Email/Password
+  - Google Provider
+
+# Learn
+
+## Setup Firebase Client-side and Server-side
+
+Client-side (firebase/client.ts) is for user interactions (e.g., login, file upload).
+Server-side (firebase/server.ts) is for admin tasks (e.g., managing users, database
---
+++ firebase/client.ts
@@ -0,0 +1,30 @@
+import { getApps, initializeApp } from 'firebase/app';
+import { Auth, getAuth } from 'firebase/auth';
+import { FirebaseStorage, getStorage } from 'firebase/storage';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_CONFIG_API_KEY,
+  authDomain: process.env.FIREBASE_CONFIG_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_CONFIG_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_CONFIG_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_CONFIG_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_CONFIG_APP_ID,
+};
+
+// Initialize Firebase
+const currentApps = getApps();
+let auth: Auth;
+let storage: FirebaseStorage;
+
+if (!currentApps.length) {
+  const app = initializeApp(firebaseConfig);
+  auth = getAuth(app);
+  storage = getStorage(app);
+} else {
+  const app = currentApps[0];
+  auth = getAuth(app);
+  storage = getStorage(app);
+}
+
+export { auth, storage };
